79b71a7b0bb437e4939c1ee8eed83d9b65ead7d8
aleksei <darklisik.darklisik@yandex.ru>
2024-08-02 17:53:18 +0300

    4. Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkParseWhenNamesArrayIsEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Names array is empty");
+    }
+
+    @Test
+    void checkValidateWhenNameNotContainsEqualSign() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "key1_value1";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("this name: %s does not contain the symbol '='", name);
+    }
+
+    @Test
+    void checkValidateWhenNameStartsWithEqualSign(){
+        NameLoad nameLoad = new NameLoad();
+        String name = "=key1=value1";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("this name: %s does not contain a key", name);
+    }
+
+    @Test
+    void checkValidateWhenNameEndsWithEqualSign() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "key1value1=";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("this name: %s does not contain a value", name);
+    }
+
+}
