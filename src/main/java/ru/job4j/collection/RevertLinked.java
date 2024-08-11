d635156452487351e1f24a75cf45663990939dc1
aleksei <darklisik.darklisik@yandex.ru>
2024-08-11 12:22:10 +0300

    6. Перевернуть связанный список [#161 #508476]

--- src/main/java/ru/job4j/collection/RevertLinked.java
+++ src/main/java/ru/job4j/collection/RevertLinked.java
@@ -7,34 +7,33 @@ public class RevertLinked<T> implements Iterable<T> {
     private Node<T> head;
 
     public void add(T value) {
-        Node<T> node = new Node<T>(value, null);
-        if (head == null) {
-            head = node;
+        if (head != null) {
+            Node<T> tail = head;
+            while (tail.next != null) {
+                tail = tail.next;
+            }
+            tail.next = new Node<>(value, null);
             return;
         }
-        Node<T> tail = head;
-        while (tail.next != null) {
-            tail = tail.next;
-        }
-        tail.next = node;
+        head = new Node<>(value, null);
     }
 
     public boolean revert() {
-        if (head == null || head.next == null) {
-            return false;
+        if (head != null && head.next != null) {
+            Node<T> tail = head;
+            Node<T> next = head.next;
+            tail.next = null;
+            while (next.next != null) {
+                Node<T> cont = tail;
+                tail = next;
+                next = tail.next;
+                tail.next = cont;
+            }
+            next.next = tail;
+            head = next;
+            return true;
         }
-        Node<T> tail = head;
-        Node<T> next = head.next;
-        tail.next = null;
-        while (next.next != null) {
-            Node<T> cont = tail;
-            tail = next;
-            next = tail.next;
-            tail.next = cont;
-        }
-        next.next = tail;
-        head = next;
-        return true;
+        return false;
     }
 
     @Override
