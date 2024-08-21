11726e4fd3c513b845b483c0e936d208a3d68699
aleksei <darklisik.darklisik@yandex.ru>
2024-08-21 19:39:17 +0300

    10. Реализовать собственную структуру данных - HashMap [#1008 #510051]

--- src/main/java/ru/job4j/map/NonCollisionMap.java
+++ src/main/java/ru/job4j/map/NonCollisionMap.java
@@ -96,7 +96,7 @@ public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
     }
 
     private boolean isEqualsHashCodesOfKeys(K key, K entryKey) {
-        return Objects.hashCode(key) == Objects.hashCode(entryKey);
+        return Objects.equals(key, entryKey);
     }
 
     private void expand() {
