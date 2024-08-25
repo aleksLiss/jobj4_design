d59f10bcd2bcd1c4c71d81a68bc702edda2e6044
aleksei <darklisik.darklisik@yandex.ru>
2024-08-25 13:52:43 +0300

    5.2. BufferedOutputStream [#252490 #510239]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -2,11 +2,11 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class LogFilter {
     private final String file;
+
     public LogFilter(String file) {
         this.file = file;
     }
