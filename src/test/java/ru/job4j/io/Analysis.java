5c933ffd663b52b0c2932d6f5f1e49b1346b7bf5
aleksei <darklisik.darklisik@yandex.ru>
2024-08-26 19:54:03 +0300

    7. Анализ доступности сервера [#859 #510701]

--- src/test/java/ru/job4j/io/Analysis.java
+++ src/test/java/ru/job4j/io/Analysis.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.StringJoiner;
 
 public class Analysis {
     public void unavailable(String source, String target) {
@@ -9,27 +8,23 @@ public class Analysis {
              BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
             String readLine;
             boolean badFlag = false;
-            boolean goodFlag = false;
+            String result = "";
             String formatSourceFile = source.split("\\.")[1]
                     + System.lineSeparator()
                     + System.lineSeparator();
             writer.write(formatSourceFile);
-            StringJoiner joiner = new StringJoiner(";", "", ";" + System.lineSeparator());
             while ((readLine = reader.readLine()) != null) {
                 String[] arr = readLine.split(" ");
                 if (("400".equals(arr[0]) || "500".equals(arr[0])) && !badFlag) {
-                    joiner.add(arr[1]);
+                    result += arr[1] + ";";
                     badFlag = true;
                 }
                 if (("200".equals(arr[0]) || "300".equals(arr[0])) && badFlag) {
-                    joiner.add(arr[1]);
-                    goodFlag = true;
-                }
-                if (goodFlag && badFlag) {
+                    result += arr[1] + ";";
+                    result += System.lineSeparator();
+                    writer.write(result);
+                    result = "";
                     badFlag = false;
-                    goodFlag = false;
-                    writer.write(joiner.toString());
-                    joiner = new StringJoiner(";", "", ";" + System.lineSeparator());
                 }
             }
         } catch (IOException ex) {
