c25b1d0bf1edf52e060ad6ef85b1c3425e76a60c
aleksei <darklisik.darklisik@yandex.ru>
2024-08-26 21:17:09 +0300

    8. Тестирование IO [#173905 #510722]

--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -15,12 +15,12 @@ class AnalysisTest {
         File source = tempDir.resolve("source.log").toFile();
         File target = tempDir.resolve("target.csv").toFile();
         try (PrintWriter output = new PrintWriter(source)) {
-            output.println("200 10:56:01\n" +
-                    "500 10:57:01\n" +
-                    "400 10:58:01\n" +
-                    "300 10:59:01\n" +
-                    "500 11:01:02\n" +
-                    "200 11:02:02");
+            output.println("200 10:56:01\n"
+                    + "500 10:57:01\n"
+                    + "400 10:58:01\n"
+                    + "300 10:59:01\n"
+                    + "500 11:01:02\n"
+                    + "200 11:02:02");
         }
         new Analysis().unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder result = new StringBuilder();
@@ -35,12 +35,12 @@ class AnalysisTest {
         File source = tempDir.resolve("source.log").toFile();
         File target = tempDir.resolve("target.csv").toFile();
         try (PrintWriter output = new PrintWriter(source)) {
-            output.println("200 10:56:01\n" +
-                    "500 10:57:01\n" +
-                    "400 10:58:01\n" +
-                    "500 10:59:01\n" +
-                    "400 11:01:02\n" +
-                    "300 11:02:02");
+            output.println("200 10:56:01\n"
+                    + "500 10:57:01\n"
+                    + "400 10:58:01\n"
+                    + "500 10:59:01\n"
+                    + "400 11:01:02\n"
+                    + "300 11:02:02");
         }
         new Analysis().unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder result = new StringBuilder();
