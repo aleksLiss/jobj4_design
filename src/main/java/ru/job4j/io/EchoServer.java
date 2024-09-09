d2de3a7f55a56964f69bba692cd790b0391738bc
aleksei <darklisik.darklisik@yandex.ru>
2024-09-09 21:49:28 +0300

    2. Что такое Socket? [#4850 #512522]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -5,29 +5,18 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    private static boolean isClosed = false;
-
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            while (!isClosed) {
+            while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream output = socket.getOutputStream();
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
-                        if (string.contains("msg=")) {
-                            String msg = string.split("msg=")[1].split(" ")[0];
-                            if ("Exit".equals(msg)) {
-                                isClosed = true;
-                                break;
-                            }
-                            if ("Hello".equals(msg)) {
-                                output.write("Hello\r\n\r\n".getBytes());
-                            } else {
-                                msg += "\r\n\r\n";
-                                output.write(msg.getBytes());
-                            }
+                    String msg = input.readLine();
+                    if (msg != null && !msg.isEmpty()) {
+                        if (msg.contains("msg=Bye ")) {
+                            server.close();
                         }
                     }
                     output.flush();
@@ -36,3 +25,4 @@ public class EchoServer {
         }
     }
 }
+
