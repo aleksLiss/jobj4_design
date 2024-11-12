3e1e2373c8179e6b0260388610fbb4c8d107007c
aleksei <darklisik.darklisik@yandex.ru>
2024-11-12 19:34:05 +0300

    3. Что такое TDD? [#4918]

---
+++ src/test/java/ru/job4j/ood/tdd/Cinema3DTest.java
@@ -0,0 +1,55 @@
+package ru.job4j.ood.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class Cinema3DTest {
+    @Test
+    public void whenBuyThenGetTicket() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket).isEqualTo(new Ticket3D());
+    }
+
+    @Test
+    public void whenAddSessionThenItExistsBetweenAllSessions() {
+        Cinema cinema = new Cinema3D();
+        Session session = new Session3D();
+        cinema.add(session);
+        List<Session> sessions = cinema.find(data -> true);
+        assertThat(sessions).contains(session);
+    }
+
+    @Test
+    public void whenBuyOnInvalidRowThenGetException() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        assertThatThrownBy(() -> cinema.buy(account, -1, 1, date))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void whenBuyOnInvalidColumnThenGetException() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        assertThatThrownBy(() -> cinema.buy(account, 2, -2, date))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void whenBuyOnInvalidDateThenGetException() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = new Calendar.Builder().setDate(1800, 11, 22).build();
+        assertThatThrownBy(() -> cinema.buy(account, 2, 2, date))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
